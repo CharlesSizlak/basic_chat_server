7b0c91beea9b9c3504a3ddbc2ffef48a16d56d15
Charles Kuhn <charlesikuhn@gmail.com>
2020-07-31 13:06:48 -0400

    removed a bunch of comments used when initially building the program

--- server.py
+++ server.py
@@ -4,17 +4,13 @@ import cmd
 import threading
 from time import sleep
 
-#first we need to open up a server that will accept incoming connections and bind them to a socket
-#information shared in that socket needs to be turned into strings if possible and sent as outbound packets to every other existing socket
-#so we somehow need to understand where messages came from and where other conversations exist
 
 to_send = {}
 broken_connections = set()
 active_connections = []
 connection_manager = None
 
-#TODO check to see if the connection exists before we send anything along it.
-#TODO finish up the rest of the commands and find more bugs
+#TODO when we recieve nonsense bytes bad things happen and the server errors out.
 class MessageManager(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
@@ -44,12 +40,6 @@ class ConnectionThread(threading.Thread):
         active_connections.append(self)
         
     def run(self):
-        #first we will populate to_send with a unique identifier as the key associated with this connection
-        #that is just going to have an empty list
-        #if we recieve information over our actual connection we'll add to everyone elses list
-        #the message we got
-        #then as part of the while loop we check our list for anything in the list to send, then we clear
-        #out our list
         to_send[self] = []
         while True:
             data = self.clientsocket.recv(12)
@@ -57,20 +47,7 @@ class ConnectionThread(threading.Thread):
             if not len(data):
                 return
             to_send[self].append(data)
-        #who is running
-        #we have our class of ServerMan running
-        #we have a thread of ConnectionHandler who is going to be blocked
-        #we have our thread of ConnectionThread going which is blocked on recv
-        #we have our client running who doesnt know what messages he is supposed to be getting
-        
-
-        # We can spin up a new thread where all he does is check to see if there are messages
-        # that need to be sent to people. Then because the clientsocket is a property of self
-        # in the class of ConnectionThread we could have another method exist in the class
-        # to send the messages we need to send, since we can run that while the actual thread
-        # himself is busy being sleepy.
     def send(self, message):
-        #send the message along the clientsocket
         self.clientsocket.send(message.encode("utf-8"))
 
     def say_goodbye(self):
@@ -108,7 +85,6 @@ class ServerMan(cmd.Cmd):
     prompt = ":D : "
 
     def do_openserver(self, arg):
-        #open the server and start accepting incoming connections.
         global connection_manager
         if connection_manager == None:
             ConnectionHandler().start()
@@ -116,7 +92,6 @@ class ServerMan(cmd.Cmd):
             print("You've already got a server running.")
 
     def do_closeserver(self, arg):
-        #close the server nicely by saying goodbye, close the program
         global connection_manager
         for connection in active_connections:
             if connection.is_alive():
@@ -128,15 +103,14 @@ class ServerMan(cmd.Cmd):
         print("Server shut down.")
 
     def do_clearconnections(self, arg):
-        #tell all the current sockets goodbye
         for connection in active_connections:
             if connection.is_alive():
                 connection.say_goodbye()
         active_connections.clear()
         print("Connections closed.")
-            
+    
+    #TODO make this work
     def do_messagehistory(self, arg):
-        #print the message history
         pass
 
     def do_threadcount(self, arg):
